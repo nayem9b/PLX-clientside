b467650066b7d3f71685ce2ff601459bd78e18f3
nayem9b <razwanniam1@gmail.com>
2023-01-07 11:19:08 +0600

    my purchase fetched

--- src/Components/MyPurchases/MyPurchases.js
+++ src/Components/MyPurchases/MyPurchases.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../Context/UserContext";
 
 const MyPurchases = () => {
+  const [myPurchase, setMyPurchase] = useState([]);
+  const { user } = useContext(AuthContext);
+  useEffect(() => {
+    fetch(`http://localhost:5000/mypurchase?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, [user.email]);
   return (
     <div>
       <h1>This is my purchase</h1>
