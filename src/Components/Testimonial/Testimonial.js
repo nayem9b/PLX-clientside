2105cf23eccd74a72b17bc2d995a040cab728dff
nayem9b <razwanniam1@gmail.com>
2023-01-09 21:56:45 +0600

    testimonial updated

--- src/Components/Testimonial/Testimonial.js
+++ src/Components/Testimonial/Testimonial.js
@@ -20,11 +20,11 @@ const Testimonial = () => {
                 </svg>
                 <p class='leading-relaxed mb-6'>
                   <span className='text-orange-400 text-xl font-bold italic'>
-                    Anonna
+                    PLX
                   </span>{" "}
-                  is the best cook in our town. And her dishes prices are
-                  reasonable and heartwarming to eat. I tend to order food from
-                  her very often
+                  is the best exchange store in our town. We have been using PLX
+                  for a long time and never faced an issue. Since PLX is backed
+                  by B2B method, we have found no anomaly with payment.
                 </p>
                 <a class='inline-flex items-center'>
                   <div className='avatar'>
@@ -53,13 +53,14 @@ const Testimonial = () => {
                   <path d='M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z'></path>
                 </svg>
                 <p class='leading-relaxed mb-6'>
-                  Apu onek valo ranna kore. Ami prayoi order dei. Tar hater
-                  Chicken roast ranna ta sobcheye joss
+                  The Payment system is really good. And it uses Stripe's
+                  payment gateway. And as it is secure, We have no issue with
+                  other consumers.
                   <span className='text-orange-400 text-xl font-bold italic mr-1 ml-2'>
-                    Anonna
+                    PLX
                   </span>
                   {"    "}
-                  apu er dish gulor dam o reasonable
+                  is the best at its own standerd
                 </p>
                 <a class='inline-flex items-center'>
                   <div className='avatar'>
